7cddb321de95b30dcab4b5750e0fcb73c6ebb1ba
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-08-14 02:07:24 -0500

    Inital commit

---
+++ Is it Prime/Is it Prime/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Is it Prime
+//
+//  Created by Hien Le on 8/14/17.
+//  Copyright © 2017 Hien Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // This is a comment
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
