2a1e453f34a0793d555e758ff77654df316b600f
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-08-16 18:17:08 -0500

    To Do List App

---
+++ Controlling The Keyboard/Controlling The Keyboard/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Controlling The Keyboard
+//
+//  Created by Hien Le on 8/16/17.
+//  Copyright © 2017 Hien Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true 
+    }
+
+
+}
+
---
+++ To Do List/To Do List/SecondViewController.swift
@@ -0,0 +1,66 @@
+//
+//  SecondViewController.swift
+//  To Do List
+//
+//  Created by Hien Le on 8/16/17.
+//  Copyright © 2017 Hien Le. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet var itemTextField: UITextField!
+    
+    @IBAction func add(_ sender: Any) {
+        
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        var items:[String]
+        
+        if let tempItems = itemsObject as? [String] {
+            
+            items = tempItems
+            
+            items.append(itemTextField.text!)
+            
+        } else {
+            
+            items = [itemTextField.text!]
+            
+        }
+        
+        UserDefaults.standard.set(items, forKey: "items")
+        
+        itemTextField.text = ""
+        
+        
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
