f8e8e4ca234be1d217c1932ab744032f9ec8873d
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-09-07 06:00:01 -0500

    add Location Aware, working with CLLocation

---
+++ Maps/Maps/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Maps
+//
+//  Created by Hien Le on 8/18/17.
+//  Copyright © 2017 Hien Le. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet var map: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let latitude: CLLocationDegrees = 10.7546664
+        let longtitude: CLLocationDegrees = 106.4150352
+        // zoom of the map
+        let latDelta: CLLocationDegrees = 0.05
+        let lonDelta: CLLocationDegrees = 0.05
+        
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longtitude)
+        
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        
+        map.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.title = "Saigon"
+        
+        annotation.subtitle = "One day I'll go here ... "
+        
+        annotation.coordinate = location
+        
+        map.addAnnotation(annotation)
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
