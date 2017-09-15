9adc13ee7d7968ce9df74627085265609b289544
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-09-14 23:16:07 -0500

    Instagram Clone Firebase - Tab Bar Controller Setup

---
+++ Instagram/Instagram/HomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.swift
+//  Instagram
+//
+//  Created by Hien Le on 9/14/17.
+//  Copyright © 2017 Hien Le. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        // change color of view 
+        view.backgroundColor = UIColor.black 
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
